f0c97a2247b914637e0c598974b90bbabc073a3c
evanlarsen <evan.larsen@gmail.com>
2016-01-19 21:33:25 -0800

    fixing ajax on account

--- src/account/login.ts
+++ src/account/login.ts
@@ -1,14 +1,34 @@
 import {Redirect} from 'aurelia-router';
+import {HttpClient, json} from 'aurelia-fetch-client';
+import {autoinject} from 'aurelia-framework';
 
+@autoinject
 export class Login{
+  http: HttpClient;
   heading = 'Login';
-  username = '';
-  password = '';
+  email = 'evan.larsen@gmail.com';
+  password = 'test1234';
   rememberMe = false;
   errorMessages = [];
 
+  constructor(http: HttpClient){
+    http.configure(config => {
+      config.withBaseUrl('http://localhost:54850/api/')
+        .withDefaults({
+          headers: { 'Accept': 'application/json' }
+        })
+    });
+    this.http = http;
+  }
+
   submit(){
-    if (this.username === '') { this.errorMessages.push('Username is required.');}
+    console.log({email: this.email, password: this.password});
+    //if (this.email === '') { this.errorMessages.push('Username is required.');}
+    this.http.fetch('account/login', {
+      method: 'post',
+      body: json({ email: this.email, password: this.password })
+    }).then(response => response.json())
+    .then(data => console.log(data));
   }
 
   facebookLogin(){
